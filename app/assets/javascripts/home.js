d1f5ef9e24121f179e9e47e3b6d8128198ee6a4e
Sophia Pignatelli <pignatelli@fordham.edu>
2017-04-11 11:50:45 -0400

    methods on spot model

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,2 +1,54 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+$(document).ready(function() {
+  initMap();
+  // all the code you want to run when the page loads
+  // $("#whatever").on("click", function(){
+  //
+  // })
+  // this should include *binding* event handlers to nodes
+
+})
+
+var map, infoWindow;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 15
+  });
+  infoWindow = new google.maps.InfoWindow;
+
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var latitude = position.coords.latitude;
+      var longitude = position.coords.longitude;
+      var coords = new google.maps.LatLng(latitude, longitude);
+      var directionsService = new google.maps.DirectionsService();
+      var directionsDisplay = new google.maps.DirectionsRenderer();
+      var mapOptions =
+      {
+        zoom: 15,
+        center: coords,
+        mapTypeControl: true,
+        navigationControlerOptions:
+        {
+          style: google.maps.NavigationControlStyle.SMALL
+        },
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        map = new google.maps.Map( document.getElementById("map"), mapOptions);
+        directionsDisplay.setMap(map);
+        directionsDisplay.setPanel(document.getElementById('panel'));
+        var request = {
+          origin: coords,
+          destination: 'BT42 1FL',
+          travelMode: google.maps.DirectionsTravelMode.DRIVING
+        };
+
+        directionsService.route(request, function (response, status) {
+          if (status == google.maps.DirectionsStatus.OK) {
+            directionsDisplay.setDirections(response);
+            }
+          });
+        });
+      }
+}
--- app/models/spot.rb
+++ app/models/spot.rb
@@ -9,4 +9,13 @@ class Spot < ApplicationRecord
 	def checkout
 		self.checkout = true
 	end
+
+	def destroy_spot
+		if self.checkout == true && self.updated_at < Time.current - (5*60)
+			self.destroy
+		end
+	end
+
+	# def spots_within_radius
+	# end
 end
