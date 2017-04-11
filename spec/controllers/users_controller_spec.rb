4065613673292ebfd44bf54d7bcf14ede466965f
Sophia Pignatelli <pignatelli@fordham.edu>
2017-04-11 10:19:26 -0400

    add user model and controller tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -5,11 +5,11 @@ RSpec.describe UsersController, type: :controller do
 	let!(:user) {User.create!(username:"johndoe",email:"johndoe@email.com", password:"123456")}
   	describe "GET #show" do
 	    it "redirects when user is not signed in" do
-	      get :show, {id: user.id}
+	      get :show, params: {id: user.id}
 	      expect(response).to have_http_status 302
 	    end
 	    it "redirects when user is not signed in" do
-	    	get :show, {id: user.id}
+	    	get :show, params: {id: user.id}
 	    	expect(response).to redirect_to(new_user_session_path)
 	    end
 	 
