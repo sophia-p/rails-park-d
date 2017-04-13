bc12e928e99d5d01f47433b191b281e63ceb96d7
Sophia Pignatelli <pignatelli@fordham.edu>
2017-04-13 10:07:10 -0400

    add tests for signed in user

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -5,11 +5,11 @@ RSpec.describe UsersController, type: :controller do
   	describe "GET #show" do
 
 	    it "redirects when user is not signed in" do
-	      get :show, params: {id: nil}
+	      get :show, params: {id: user.id}
 	      expect(response).to have_http_status 302
 	    end
 	    it "redirects when user is not signed in" do
-	    	get :show, params: {id: nil}
+	    	get :show, params: {id: user.id}
 	    	expect(response).to redirect_to(new_user_session_path)
 	    end	 
   	end
