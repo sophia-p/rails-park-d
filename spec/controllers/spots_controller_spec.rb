a56f3a0b357e410e728d9e710d6b75105db6351f
Sophia Pignatelli <pignatelli@fordham.edu>
2017-04-13 14:27:10 -0400

    dest validations

--- spec/controllers/spots_controller_spec.rb
+++ spec/controllers/spots_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe SpotsController, type: :controller do
+	let(:spot) {FactoryGirl.create(:spot)}
 	let(:user) {FactoryGirl.create(:user)}
   	describe "GET #index" do
 	    it "redirects when user is not signed in" do
@@ -10,25 +11,41 @@ RSpec.describe SpotsController, type: :controller do
 	    it "redirects when user is not signed in" do
 	    	get :index
 	    	expect(response).to redirect_to(new_user_session_path)
-	    end	 
+	    end	
+	    it 'returns JSON' do
+	  		get :index
+  			expect(response.content_type).to eq("text/html")
+		end 
   	end
 
-  	describe "signed in user" do
-  		before(:each) do 
-  			login_with user(FactoryGirl.create(:user, username:"janedoe", email:"janedoe@email.com"))
-  		end
-  	it "returns http success" do
-  			FactoryGirl.create_list(:spot, 5)
-	  		# login_with user
-	  		get :index, format: :json
-	  		parse_json = JSON.parse(response.body)
-	  		expect(parse_json["spots"]).to_not be_nil
+
+	describe "GET#show" do
+		context "user is not signed in" do
+	  		it "redirects to sign in" do
+		  		get :show, params: {id: spot.id}
+		  		expect(response).to redirect_to(new_user_session_path)
+			end
 		end
-  		it "allows access to signed in user" do
-	  		# login_with user
-	  		get :index, format: :json
-	  		expect(response).to have_http_status(:success)
-	  	end
-  	end
+	end
+	describe "GET#new" do
+		context "user is not signed in" do
+	  		it "redirects to sign in" do
+		  		get :new
+		  		expect(response).to redirect_to(new_user_session_path)
+			end
+		end
+	end	
+
+	describe "GET#create" do
+		context "user is not signed in" do
+	  		it "redirects to sign in" do
+		  		get :new
+		  		expect(response).to redirect_to(new_user_session_path)
+			end
+		end
+	end
+
+
+
 
 end
